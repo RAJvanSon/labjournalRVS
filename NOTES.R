f5e7feff58bd4647f4210a525da45a5ae2772625
RAJvanSon <rene.vanson@ru.nl>
2024-09-11 12:09:22 +0200

    3

---
+++ NOTES.R
@@ -0,0 +1,11 @@
+
+
+###this R code could be used to include an image (instead of the markdown way)]
+
+```{r, echo=FALSE, out.width="20%", out.height="20%", fig.cap="Social Networks"}
+#install.packages("knitr")
+require(knitr)
+knitr::include_graphics("/Users/jannevanheesch/Downloads/image-asset.jpeg")
+```
+
+###look at openAlex for interdisciplinarity question. 
